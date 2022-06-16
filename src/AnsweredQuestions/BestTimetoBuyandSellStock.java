0f56164a6355dfb7608c241054f7b5d9aeab1c7e
kamran6433 <kamran_12@icloud.com>
2022-06-16 17:53:53 +0100

    Added two packages for this repository and added the answered and unanswered questions in them.

---
+++ src/AnsweredQuestions/BestTimetoBuyandSellStock.java
@@ -0,0 +1,19 @@
+package AnsweredQuestions;
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+public class BestTimetoBuyandSellStock {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int costPrice = prices[0];
+
+        for (int i = 0; i < prices.length; i++) {
+            // Using Math.min to compare the costPrice with every price in the array
+            costPrice = Math.min(costPrice, prices[i]);
+            // Using Math.max to compare the maxProfit with
+            // (every price in the array) - (costPrice)
+            maxProfit = Math.max(maxProfit, prices[i] - costPrice);
+        }
+        return maxProfit;
+    }
+}
