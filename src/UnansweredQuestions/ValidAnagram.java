d0d6b2da8edf6850185192be97459e1ec95f54bf
kamran6433 <kamran_12@icloud.com>
2022-06-18 10:34:27 +0100

    Updated the answered and unanswered questions.

---
+++ src/UnansweredQuestions/ValidAnagram.java
@@ -0,0 +1,27 @@
+package UnansweredQuestions;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+        isAnagram("poop","popo");
+    }
+
+    public static boolean isAnagram(String s, String t) {
+
+        char[] stringS = s.toCharArray();
+        char[] stringT = t.toCharArray();
+
+        for (int i = 0; i < stringS.length; i++) {
+            System.out.print(stringS[i]);
+        }
+
+        for (int j = 0; j < stringT.length; j++) {
+            System.out.print(stringT[j]);
+        }
+
+        return false;
+
+    }
+}
