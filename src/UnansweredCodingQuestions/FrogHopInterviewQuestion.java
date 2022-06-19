9fa915c377e10f1aa0567b35456eb005896939ea
kamran6433 <kamran_12@icloud.com>
2022-06-19 11:37:52 +0100

    Updated the answered and unanswered questions.

---
+++ src/UnansweredCodingQuestions/FrogHopInterviewQuestion.java
@@ -0,0 +1,18 @@
+package UnansweredCodingQuestions;
+
+public class FrogHopInterviewQuestion {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int InterviewQuestion(int n, int m) {
+
+
+
+
+
+        return -1;
+    }
+
+}
